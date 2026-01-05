2f7f7f6fb375f8aa270aba60798c408debba04ab
Pawan Lalchand <pawan103@gmail.com>
2026-01-05 16:23:52 +0800

    Added index.jsx

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,211 @@
+import React, { useContext, useRef, useState } from "react";
+import {
+  Box,
+  Container,
+  IconButton,
+  Grid,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+import BrandContainer from "./components/BrandContainer";
+import FindUs from "./components/FindUs";
+import { BrandsContext } from "../../context/BrandsContext";
+// import HeaderImg from "../../assets/images/header-img.webp";
+import HeaderImg from "../../assets/images/header.png";
+import ArrowHead from "../../assets/icons/arrow-head.svg";
+import TradLuxe from "../../assets/logo/tradlogo.svg";
+import CustomTitle from "../../components/CustomTitle";
+
+const HomePage = () => {
+  const [brands] = useContext(BrandsContext);
+  const tabMatches = useMediaQuery("(min-width:900px)");
+
+  const containerRef = useRef(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [startX, setStartX] = useState(0);
+
+  const handleMouseDown = (e) => {
+    setIsDragging(true);
+    setStartX(e.clientX + containerRef.current.scrollLeft);
+  };
+
+  const handleMouseMove = (e) => {
+    if (!isDragging) return;
+    const scrollX = startX - e.clientX;
+    containerRef.current.scrollLeft = scrollX;
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  const handleMouseLeave = () => {
+    setIsDragging(false);
+  };
+
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          height: "100vh",
+          overflowX: "hidden",
+          position: "relative",
+        }}
+        className="page-fade"
+      >
+        <img
+          src={HeaderImg}
+          alt="Header Image"
+          style={{
+            width: "100%",
+            maxHeight: tabMatches ? "100%" : "600px",
+            objectFit: "cover",
+            height: tabMatches ? "100%" : "600px",
+          }}
+          loading="eager"
+        />
+        <Box
+          sx={{
+            position: "absolute",
+            bottom: "30px",
+            left: "20%",
+            transform: "translateX(-20%)",
+            textAlign: "center",
+          }}
+        >
+          <Typography
+            variant="h3"
+            sx={{
+              fontFamily: "'Playfair Display Variable', serif",
+              fontSize: !tabMatches ? "24px" : "60px",
+              marginBottom: "100px",
+              maxWidth: "70%",
+              textAlign: "left",
+              fontWeight: 600,
+              lineHeight: "130%",
+              textTransform: "capitalize",
+              color: "#F0F0F0",
+            }}
+          >
+            Taste the World, One Treat at a Time
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            position: "absolute",
+            bottom: "10px",
+            left: "50%",
+            transform: "translateX(-50%)",
+            textAlign: "center",
+          }}
+        >
+          <Typography
+            variant="h5"
+            sx={{
+              fontFamily: "'Playfair Display Variable', serif",
+              fontSize: "18px",
+              fontWeight: 600,
+              lineHeight: "130%",
+              textTransform: "capitalize",
+              color: "#F0F0F0",
+            }}
+          >
+            Scroll to see more
+          </Typography>
+          <IconButton
+            onClick={() => {
+              const section = document.getElementById("brands");
+              if (section) {
+                section.scrollIntoView({ behavior: "smooth" });
+              }
+            }}
+            role="button"
+          >
+            <img src={ArrowHead} alt="arrow-head" loading="eager" />
+          </IconButton>
+        </Box>
+      </Box>
+
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          height: "100%",
+          my: 8,
+        }}
+      >
+        <img
+          src={TradLuxe}
+          alt="TradLuxe Image"
+          style={{ maxWidth: "100%" }}
+          loading="lazy"
+        />
+      </Box>
+
+      <Box sx={{ my: 5 }}>
+        <CustomTitle subtitle="Discover" title="Our Brands" />
+      </Box>
+
+      <Container
+        sx={{
+          position: "relative",
+        }}
+      >
+        <Box
+          id="brands"
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            my: 8,
+            overflowX: "scroll",
+            pb: 3,
+            position: "relative",
+            width: "100%",
+          }}
+          ref={containerRef}
+          onMouseDown={handleMouseDown}
+          onMouseMove={handleMouseMove}
+          onMouseUp={handleMouseUp}
+          onMouseLeave={handleMouseLeave}
+        >
+          <Grid container spacing={2}>
+            {brands.map((brand, idx) => (
+              <Grid
+                item
+                xs={12}
+                md={4}
+                key={idx}
+                sx={{ overflow: "hidden", mt: idx % 2 === 0 ? 0 : 5 }}
+              >
+                <BrandContainer
+                  img={brand.image}
+                  logo={brand.logo}
+                  flag={brand.flag}
+                  subline={brand.subline}
+                  content={brand.content}
+                  name={brand.name}
+                  idx={idx}
+                  link={brand.link}
+                  isLaunched={brand.isLaunched}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </Container>
+
+      <Container sx={{ height: "100%" }}>
+        <CustomTitle
+          subtitle="Around the World in Every Bite"
+          title="Where to find us?"
+        />
+      </Container>
+      <FindUs />
+    </>
+  );
+};
+
+export default HomePage;
