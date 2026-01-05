00b4447e229abce6ce17b9e86b6459418b0af8ed
Pawan Lalchand <pawan103@gmail.com>
2026-01-05 16:24:58 +0800

    Added index.jsx

---
+++ src/pages/product/index.jsx
@@ -0,0 +1,217 @@
+import {
+  Box,
+  Container,
+  Grid,
+  Link,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { BrandsContext } from "../../context/BrandsContext";
+import ProductsBreadCrumbs from "./components/BreadCrumbs";
+import CustomToolbar from "../../components/CustomToolbar";
+
+const Product = () => {
+  const { id } = useParams();
+  const [brands] = useContext(BrandsContext);
+
+  const [brand, setBrand] = useState({
+    bg: "",
+    shorthand: "",
+    products: [],
+  });
+
+  useEffect(() => {
+    const reqBrand = brands.filter((itm) => itm.link === id);
+    if (reqBrand?.[0]) {
+      setBrand(reqBrand?.[0]);
+    }
+  }, [brands]);
+
+  const tabMatches = useMediaQuery("(min-width:900px)");
+  return (
+    <>
+      <CustomToolbar />
+      {/* <Box sx={{ mt: 5, position: "relative" }} className="page-fade">
+        {brand.bg !== "" && (
+          <img
+            src={brand.bg}
+            alt={`brand-${brand.name}`}
+            width={"100%"}
+            style={{ maxHeight: "570px", objectFit: "cover" }}
+            loading="lazy"
+          />
+        )}
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            display: "flex",
+            alignItems: "center",
+            background: "rgba(255, 255, 255, 0.70)",
+            backdropFilter: "blur(6px)",
+            padding: tabMatches ? "1rem 1.25rem" : "0.5rem 0.75rem",
+            textAlign: "center",
+          }}
+        >
+          <Typography
+            sx={{
+              color: "#0F0F0F",
+              fontFamily: '"Playfair Display Variable", serif',
+              fontSize: tabMatches ? "4.5rem" : "1.25rem",
+              fontWeight: 500,
+              lineHeight: "120%",
+            }}
+            variant="h1"
+            component="h4"
+          >
+            {brand.shorthand} Products
+          </Typography>
+        </Box>
+      </Box> */}
+      <Container className="page-fade">
+        <ProductsBreadCrumbs name={brand.shorthand} />
+        <Typography sx={{ mb: 3 }}>
+          Showing {brand?.products?.length} Products
+        </Typography>
+        <Grid container spacing={3}>
+          {brand.products.map((product, idx) =>
+            product.link ? (
+              <Grid
+                item
+                key={idx}
+                xs={12}
+                md={4}
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: 1,
+                  textDecoration: "none",
+                }}
+                component={Link}
+                href={product.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={product?.img}
+                  alt={`product-${product.name}`}
+                  width="100%"
+                  data-aos="fade-up"
+                  loading="lazy"
+                />
+                <Typography
+                  component={"h4"}
+                  sx={{
+                    color: "#383838",
+                    fontFamily: '"Playfair Display Variable", serif',
+                    fontSize: "1.25rem",
+                    fontWeight: "600",
+                  }}
+                >
+                  {brand.shorthand}
+                </Typography>
+                <Typography
+                  component={"h3"}
+                  sx={{
+                    color: "#000",
+                    fontFamily: '"Playfair Display Variable", serif',
+                    fontSize: tabMatches ? "1.5rem" : "1.25rem",
+                    fontWeight: "400",
+                  }}
+                >
+                  {product.name}
+                </Typography>
+                <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                  <Typography
+                    sx={{
+                      color: "#383838",
+                      fontSize: "1rem",
+                      fontWeight: "700",
+                    }}
+                  >
+                    {product.price}
+                  </Typography>
+                  <Typography
+                    sx={{
+                      color: "#383838",
+                      fontSize: "1rem",
+                    }}
+                  >
+                    {product.quantity}
+                  </Typography>
+                </Box>
+              </Grid>
+            ) : (
+              <Grid
+                item
+                key={idx}
+                xs={12}
+                md={4}
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: 1,
+                }}
+              >
+                <img
+                  src={product?.img}
+                  alt={`product-${product.name}`}
+                  width="100%"
+                  data-aos="fade-up"
+                  loading="lazy"
+                />
+                <Typography
+                  component={"h4"}
+                  sx={{
+                    color: "#383838",
+                    fontFamily: '"Playfair Display Variable", serif',
+                    fontSize: "1.25rem",
+                    fontWeight: "600",
+                  }}
+                >
+                  {brand.shorthand}
+                </Typography>
+                <Typography
+                  component={"h3"}
+                  sx={{
+                    color: "#000",
+                    fontFamily: '"Playfair Display Variable", serif',
+                    fontSize: tabMatches ? "1.5rem" : "1.25rem",
+                    fontWeight: "400",
+                  }}
+                >
+                  {product.name}
+                </Typography>
+                <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                  <Typography
+                    sx={{
+                      color: "#383838",
+                      fontSize: "1rem",
+                      fontWeight: "700",
+                    }}
+                  >
+                    {product.price}
+                  </Typography>
+                  <Typography
+                    sx={{
+                      color: "#383838",
+                      fontSize: "1rem",
+                    }}
+                  >
+                    {product.quantity}
+                  </Typography>
+                </Box>
+              </Grid>
+            )
+          )}
+        </Grid>
+      </Container>
+    </>
+  );
+};
+
+export default Product;
